2c0f94ac1fcf82acdb49593392255222c2e6c62d
glezzz <alexglezk@gmail.com>
2021-03-29 18:17:00 +0200

    set up bottom nav bar & connect fragments

--- app/src/main/java/com/myproject/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/myproject/newsapp/ui/NewsActivity.kt
@@ -2,11 +2,26 @@ package com.myproject.newsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.myproject.newsapp.R
+import com.myproject.newsapp.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        val binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Connect bottom navigation view with navigation components
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
     }
 }
