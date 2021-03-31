15276e9bcbd66cb04368f476420bf4e4d8d4a503
glezzz <alexglezk@gmail.com>
2021-03-31 13:22:18 +0200

    room setup

---
+++ app/src/main/java/com/myproject/newsapp/db/ArticleDao.kt
@@ -0,0 +1,23 @@
+package com.myproject.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.myproject.newsapp.Article
+
+@Dao
+interface ArticleDao {
+
+    // insert or update an article
+    // what happens if article already exists
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    // update or insert
+    suspend fun upsert(article: Article): Long
+
+    // pass SQL query that selects all articles that this function should return
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    // Delete article
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
